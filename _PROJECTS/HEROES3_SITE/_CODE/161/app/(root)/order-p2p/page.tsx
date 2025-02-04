14bc01adf3dbc8d8ca60d0b3d3b72c1a0eabcd11
PC1 <umdom2@gmail.com>
2025-02-04 11:46:20 +0300

    edit

---
+++ _PROJECTS/HEROES3_SITE/_CODE/161/app/(root)/order-p2p/page.tsx
@@ -0,0 +1,30 @@
+"use server";
+import { Container } from '@/components/container';
+import { prisma } from '@/prisma/prisma-client';
+import { redirect } from 'next/navigation';
+import React, { Suspense } from "react";
+import Loading from "@/app/(root)/loading";
+import { getUserSession } from "@/components/lib/get-user-session";
+import {OrderP2P} from "@/components/OrderP2P";
+
+export default async function BuyPayPointsPage() {
+    const session = await getUserSession();
+
+    if (!session) {
+        return redirect('/not-auth');
+    }
+
+    const user = await prisma.user.findFirst({ where: { id: Number(session?.id) } });
+
+    if (!user) {
+        return redirect('/not-auth');
+    }
+
+    return (
+        <Container className="w-[100%]">
+            <Suspense fallback={<Loading />}>
+                <OrderP2P user={user} />
+            </Suspense>
+        </Container>
+    );
+}
