40d63fd2616f50a9e24d46e1d95cb88710523e7e
PC1 <umdom2@gmail.com>
2025-02-22 04:38:55 +0300

    edit

---
+++ _PROJECTS/HEROES3_SITE/_CODE/guard.tsx
@@ -0,0 +1,42 @@
+export async function gameUserBetDelete(gameUserBetId: number) {
+    try {
+        const currentUser = await getUserSession();
+
+        if (!currentUser) {
+            console.error('Пользователь не найден');
+            throw new Error('Пользователь не найден');
+        }
+
+        // Найдите ставку в базе данных
+        const gameUserBet = await prisma.gameUserBet.findUnique({
+            where: { id: Number(gameUserBetId) },
+        });
+
+        if (!gameUserBet) {
+            console.error('Ставка не найдена');
+            throw new Error('Ставка не найдена');
+        }
+
+        // Проверьте, является ли текущий пользователь создателем ставки
+        if (gameUserBet.gameUserBet1Id !== Number(currentUser.id)) {
+            console.error('У вас нет прав для удаления этой ставки');
+            throw new Error('У вас нет прав для удаления этой ставки');
+        }
+
+        // Проверьте, что статус ставки - OPEN
+        if (gameUserBet.statusUserBet !== GameUserBetStatus.OPEN) {
+            console.error('Ставку можно удалить только в статусе OPEN');
+            throw new Error('Ставку можно удалить только в статусе OPEN');
+        }
+
+        // Удалите ставку
+        await prisma.gameUserBet.delete({
+            where: { id: Number(gameUserBetId) },
+        });
+
+        console.log('Ставка успешно удалена');
+    } catch (error) {
+        console.error('Ошибка при удалении ставки:', error);
+        throw new Error('Не удалось удалить ставку');
+    }
+}
