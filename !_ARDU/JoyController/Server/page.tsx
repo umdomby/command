99025615ffa2b87df6e44d324ef4da12019f484c
umdomby <umdomby@yandex.ru>
2026-03-03 17:07:36 +0300

    edit

---
+++ !_ARDU/JoyController/Server/page.tsx
@@ -0,0 +1,8 @@
+https://drive.google.com/file/d/1QxcXWWD8mstxL7pTcq2_gSAx0z_QPv2r/view?usp=sharing   exe
+    https://drive.google.com/file/d/1g3oitT89tFQ134Ds4EdTyuZAGdbqeU3j/view?usp=sharing   rar
+        это ссылки на программу
+\\wsl.localhost\Ubuntu-24.04\home\umdom\projects\prod\docker-ardu\app\(root)\joy\control\page.tsx это страница
+клиентский компонент
+\\wsl.localhost\Ubuntu-24.04\home\umdom\projects\prod\docker-ardu\components\joy\joy-control.tsx сделай страницу с сылками и описанием программы. лицензия триал - 1 час в день, лицензия на год лицензия на два года и пожизненная лицензия. Для входа в программу нужно зарегистрироваться на  https://ardu.live/register (упоминается если вы регистриуетесь черз кнопку google вы должны чиерез https://ardu.live/profile/edit  установить пароль) вход в программ у для получения триал и других лицензий нужно вводить свой емел и пароль
+    опиши возможности программы
+вот код ее в файлах подробно, и дай пользователю выбрать язык из списка русский английский немецкий французкий китайский итальянский
